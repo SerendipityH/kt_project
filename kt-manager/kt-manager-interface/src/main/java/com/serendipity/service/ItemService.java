83d7ed7163812d3624f703c243a5e0ac902dfa7a
SerendipityH <2274734747@qq.com>
2019-03-07 22:47:30 +0800

    后台新增商品功能

--- kt-manager/kt-manager-interface/src/main/java/com/serendipity/service/ItemService.java
+++ kt-manager/kt-manager-interface/src/main/java/com/serendipity/service/ItemService.java
@@ -1,10 +1,13 @@
 package com.serendipity.service;
 
 import com.serendipity.common.pojo.EasyUIDataGridResult;
+import com.serendipity.common.utils.E3Result;
 import com.serendipity.pojo.TbItem;
 
 public interface ItemService {
 	TbItem geTbItemById(long itemId);
 
 	EasyUIDataGridResult getItemList(int page, int rows);
+	
+	E3Result addItem(TbItem item,String desc);
 }
