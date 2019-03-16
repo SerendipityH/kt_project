b146c07ed716c5a4063b99d10f3f3b78f81c91ee
SerendipityH <2274734747@qq.com>
2019-03-16 22:46:39 +0800

    solr

---
+++ kt-search/kt-search-service/src/main/java/com/serendipity/search/SearchDao.java
@@ -0,0 +1,63 @@
+package com.serendipity.search;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.serendipity.common.pojo.SearchItem;
+import com.serendipity.common.pojo.SearchResult;
+
+@Repository
+public class SearchDao {
+
+	@Autowired
+	private SolrServer solrServer;
+
+	/**
+	 * 根据查询条件查询索引库
+	 * 
+	 * @param solrQuery
+	 * @return
+	 * @throws SolrServerException
+	 */
+	public SearchResult search(SolrQuery solrQuery) throws SolrServerException {
+		QueryResponse query = solrServer.query(solrQuery);
+		SolrDocumentList solrDocumentList = query.getResults();
+		long numFound = solrDocumentList.getNumFound();
+		SearchResult result = new SearchResult();
+		result.setRecordCount(numFound);
+		Map<String, Map<String, List<String>>> highlighting = query.getHighlighting();
+		List<SearchItem> itemList = new ArrayList<>();
+		for (SolrDocument solrDocument : solrDocumentList) {
+			SearchItem item = new SearchItem();
+			item.setId((String) solrDocument.get("id"));
+			item.setCategory_name((String) solrDocument.get("item_category_name"));
+			item.setImage((String) solrDocument.get("item_image"));
+			item.setPrice((long) solrDocument.get("item_price"));
+			item.setSell_point((String) solrDocument.get("item_sell_point"));
+			// 取高亮显示
+			List<String> list = highlighting.get(solrDocument.get("id")).get("item_title");
+			String title = "";
+			if (list != null && list.size() > 0) {
+				title = list.get(0);
+			} else {
+				title = (String) solrDocument.get("item_title");
+			}
+			item.setTitle(title);
+			// 添加到商品列表
+			itemList.add(item);
+		}
+		result.setItemList(itemList);
+		// 返回结果
+		return result;
+	}
+}
