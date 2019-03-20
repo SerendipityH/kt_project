8b70d3527c23fca47fcade5fb4e33b24fd803e96
SerendipityH <2274734747@qq.com>
2019-03-20 22:51:20 +0800

    activemq整合spring

---
+++ kt-search/kt-search-service/src/test/java/com/serendipity/activemq/MessageConsumer.java
@@ -0,0 +1,14 @@
+package com.serendipity.activemq;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MessageConsumer {
+	@Test
+	public void msgConsumer() throws Exception{
+		//初始化spring容器
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("classpath:spring/applicationContext-activemq.xml");
+		System.in.read();
+	}
+}
