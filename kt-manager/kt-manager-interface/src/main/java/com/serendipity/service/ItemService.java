f054494daeb038e4d06d615833c113d2c805df2e
SerendipityH <2274734747@qq.com>
2019-03-23 17:23:42 +0800

    商品详情页

--- kt-manager/kt-manager-interface/src/main/java/com/serendipity/service/ItemService.java
+++ kt-manager/kt-manager-interface/src/main/java/com/serendipity/service/ItemService.java
@@ -3,6 +3,7 @@ package com.serendipity.service;
 import com.serendipity.common.pojo.EasyUIDataGridResult;
 import com.serendipity.common.utils.E3Result;
 import com.serendipity.pojo.TbItem;
+import com.serendipity.pojo.TbItemDesc;
 
 public interface ItemService {
 	TbItem geTbItemById(long itemId);
@@ -10,5 +11,7 @@ public interface ItemService {
 	EasyUIDataGridResult getItemList(int page, int rows);
 	
 	E3Result addItem(TbItem item,String desc);
+	
+	TbItemDesc getItemDescById(long itemId);
 	
 }
--- kt-manager/kt-manager-service/src/main/java/com/serendipity/service/impl/ItemServiceImpl.java
+++ kt-manager/kt-manager-service/src/main/java/com/serendipity/service/impl/ItemServiceImpl.java
@@ -10,21 +10,26 @@ import javax.jms.Message;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.serendipity.common.jedis.JedisClient;
 import com.serendipity.common.pojo.EasyUIDataGridResult;
 import com.serendipity.common.utils.E3Result;
 import com.serendipity.common.utils.IDUtils;
+import com.serendipity.common.utils.JsonUtils;
 import com.serendipity.mapper.TbItemDescMapper;
 import com.serendipity.mapper.TbItemMapper;
 import com.serendipity.pojo.TbItem;
 import com.serendipity.pojo.TbItemDesc;
 import com.serendipity.pojo.TbItemExample;
+import com.serendipity.pojo.TbItemExample.Criteria;
 import com.serendipity.service.ItemService;
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -41,11 +46,48 @@ public class ItemServiceImpl implements ItemService {
 	@Resource
 	private Destination topicDestination;
 	
+	@Autowired
+	private JedisClient jedisClient;
+	
+	@Value("${REDIS_ITEM_PRE}")
+	private String REDIS_ITEM_PRE;
+	@Value("${ITEM_CACHE_EXPIRE}")
+	private Integer ITEM_CACHE_EXPIRE;
+	
 	@Override
 	public TbItem geTbItemById(long itemId) {
 		// TODO Auto-generated method stub
-		TbItem tbItem=itemMapper.selectByPrimaryKey(itemId);
-		return tbItem;
+		//查询缓存
+		try {
+			String json = jedisClient.get(REDIS_ITEM_PRE + ":" + itemId + ":BASE");
+			if(StringUtils.isNotBlank(json)) {
+				TbItem tbItem = JsonUtils.jsonToPojo(json, TbItem.class);
+				return tbItem;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//缓存中没有，查询数据库
+		//根据主键查询
+		//TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
+		TbItemExample example = new TbItemExample();
+		Criteria criteria = example.createCriteria();
+		//设置查询条件
+		criteria.andIdEqualTo(itemId);
+		//执行查询
+		List<TbItem> list = itemMapper.selectByExample(example);
+		if (list != null && list.size() > 0) {
+			//把结果添加到缓存
+			try {
+				jedisClient.set(REDIS_ITEM_PRE + ":" + itemId + ":BASE", JsonUtils.objectToJson(list.get(0)));
+				//设置过期时间
+				jedisClient.expire(REDIS_ITEM_PRE + ":" + itemId + ":BASE", ITEM_CACHE_EXPIRE);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return list.get(0);
+		}
+		return null;
 	}
 
 	@Override
@@ -101,5 +143,30 @@ public class ItemServiceImpl implements ItemService {
 		//返回成功
 		return E3Result.ok();
 	}
-
+	
+	//查询缓存
+	@Override
+	public TbItemDesc getItemDescById(long itemId) {
+		// TODO Auto-generated method stub
+		//查询缓存
+				try {
+					String json = jedisClient.get(REDIS_ITEM_PRE + ":" + itemId + ":DESC");
+					if(StringUtils.isNotBlank(json)) {
+						TbItemDesc tbItemDesc = JsonUtils.jsonToPojo(json, TbItemDesc.class);
+						return tbItemDesc;
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				TbItemDesc itemDesc = itemDescMapper.selectByPrimaryKey(itemId);
+				//把结果添加到缓存
+				try {
+					jedisClient.set(REDIS_ITEM_PRE + ":" + itemId + ":DESC", JsonUtils.objectToJson(itemDesc));
+					//设置过期时间
+					jedisClient.expire(REDIS_ITEM_PRE + ":" + itemId + ":DESC", ITEM_CACHE_EXPIRE);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return itemDesc;
+			}
 }
